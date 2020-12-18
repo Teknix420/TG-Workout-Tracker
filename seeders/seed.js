87a66bf7f49fa2e1253b0a7b5c607c00fcef532e
Teknix <pandapaloo@gmail.com>
2020-12-18 15:03:48 -0700

    Edited DB configuration

--- seeders/seed.js
+++ seeders/seed.js
@@ -3,12 +3,15 @@ let Seed = function () {
   let mongoose = require("mongoose");
   let db = require("../models/workout.js");
 
-  mongoose.connect("mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+  mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
   let workoutSeed = [
     {
--- server.js
+++ server.js
@@ -8,12 +8,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 app.use(require('./routes/api.js'));
 
